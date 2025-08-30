ead4d449bee223fd58c32c36e369c807803b52e4
Wei Ya <your.email@example.com>
2025-08-30 19:27:03 +0800

    gitpush

--- backend/agents/base_agent.py
+++ backend/agents/base_agent.py
@@ -1,10 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-#from backend.util.config import getConfig  # Updated import path
+from backend.util.config import getConfig  # Updated import path
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'util')))
-from config import getConfig
 from typing import Type, TypeVar
 import yaml
 from pydantic import BaseModel
